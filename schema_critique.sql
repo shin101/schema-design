816dd51fc207581cc1c62a75fa90db0b8483154d
shin101 <shinj0127@gmail.com>
2022-11-28 18:23:00 -0800

    modified

--- schema_critique.sql
+++ schema_critique.sql
@@ -1,27 +1,42 @@
--- Schema One : Outer Space
--- Lots of redundancy around orbits_around and galaxy value, might make sense to separate it out
- -- ALTER TABLE planets DROP COLUMN orbits_around,
--- ALTER TABLE planets DROP COLUMN galaxy
- -- ALTER TABLE planets ADD COLUMN orbits_id int, ADD COLUMN galaxy_id int;
--- -- (
--- --     orbits_id REFERENCES orbits_around
--- --     galaxy_id REFERENCES galaxy
--- -- )
- -- CREATE TABLE orbits_around(
---     id SERIAL PRIMARY KEY,
---     orbits_name
+Schema One : Outer Space Lots of redundancy around orbits_around
+and galaxy value,
+    might make sense to separate it out
+ALTER TABLE planets
+DROP COLUMN orbits_around,
+ALTER TABLE planets
+DROP COLUMN galaxy
+ALTER TABLE planets ADD COLUMN orbits_id int, ADD COLUMN galaxy_id int;
+
+-- (
+--     orbits_id REFERENCES orbits_around
+--     galaxy_id REFERENCES galaxy
 -- )
- -- CREATE TABLE galaxy(
---     id SERIAL PRIMARY KEY,
---     galaxy_name TEXT
--- )
- -- INSERT INTO planets(orbits_id,galaxy_id) VALUES (1,1) WHERE planets.id=1
--- INSERT INTO planets(orbits_id,galaxy_id) VALUES (2,1)
- -- INSERT INTO orbits_around(orbits_name) VALUES ('The Sun')
--- INSERT INTO orbits_around(orbits_name) VALUES ('Proxima Centauri')
--- INSERT INTO orbits_around(orbits_name) VALUES ('Gliese 876')
- -- INSERT INTO galaxy(galaxy_name) VALUES ('Milky Way');
---  Schema Two : Air Traffic
---  Separate out person and location to different tables
- --   Schema Three : Music
---   Create a separate able for album
+
+CREATE TABLE orbits_around(id SERIAL PRIMARY KEY,
+                                             orbits_name)
+CREATE TABLE galaxy(id SERIAL PRIMARY KEY,
+                                      galaxy_name TEXT)
+INSERT INTO planets(orbits_id,
+                    galaxy_id)
+VALUES (1,
+        1)
+WHERE planets.id=1
+    INSERT INTO planets(orbits_id,
+                        galaxy_id)
+VALUES (2,
+        1)
+INSERT INTO orbits_around(orbits_name)
+VALUES ('The Sun')
+INSERT INTO orbits_around(orbits_name)
+VALUES ('Proxima Centauri')
+INSERT INTO orbits_around(orbits_name)
+VALUES ('Gliese 876')
+INSERT INTO galaxy(galaxy_name)
+VALUES ('Milky Way');
+
+Schema Two : Air Traffic Separate out person
+and location to different tables 
+
+Schema Three : Music
+Create a separate able
+for album
--- schema_design.sql
+++ schema_design.sql
@@ -1,60 +1,56 @@
 -- 1. Medical Center
- -- A medical center employs several doctors
+--  A medical center employs several doctors
 -- A doctors can see many patients
 -- A patient can be seen by many doctors
 -- During a visit, a patient may be diagnosed to have one or more diseases.
- -- CREATE TABLE hospital (
---     id SERIAL PRIMARY KEY,
---     hospital_name TEXT NOT NULL,
---     hospital_id
--- )
- -- CREATE TABLE doctor (
---     id SERIAL PRIMARY KEY,
---     doc_name TEXT NOT NULL
--- doc_id INTEGER ,
--- hospital_id REFERENCES hospital
- -- )
- -- CREATE TABLE patient (
---     id SERIAL PRIMARY KEY,
---     patient_name TEXT NOT NULL,
---     doc_id INTEGER REFERENCES doctor,
---     doc TEXT REFERENCES  hospital ON DELETE SET NULL
---     diseases_id REFERENCES diseases
--- )
- -- CREATE TABLE diseases (
---     id SERIAL PRIMARY KEY,
---     diagnosis INTERGER
--- )
- --  -- 2. Craigslist
+
+ CREATE TABLE doctors (
+    id SERIAL PRIMARY KEY,
+    doc_name TEXT NOT NULL,
+    patient_id REFERENCES patients
+ )
+ CREATE TABLE patients (
+    id SERIAL PRIMARY KEY,
+    patient_name TEXT NOT NULL,
+    doc_id INTEGER REFERENCES doctors,
+    diseases_id REFERENCES diseases
+)
+ CREATE TABLE diseases (
+    id SERIAL PRIMARY KEY,
+    patient_id REFERENCES patients
+    diagnosis TEXT
+)
+  -- 2. Craigslist
 -- Design a schema for Craigslist! Your schema should keep track of the following
- -- The region of the craigslist post (San Francisco, Atlanta, Seattle, etc)
+--  The region of the craigslist post (San Francisco, Atlanta, Seattle, etc)
 -- Users and preferred region
 -- Posts: contains title, text, the user who has posted, the location of the posting, the region of the posting
 -- Categories that each post belongs to
- -- CREATE TABLE user (
---     id SERIAL PRIMARY KEY,
---     username TEXT UNIQUE,
---     preferred_region_id TEXT REFERENCES location
--- );
- -- CREATE TABLE post (
---     id SERIAL PRIMARY KEY,
---     region TEXT,
---     title TEXT,
---     content TEXT,
---     username REFERENCES user,
---     location_id INTEGER REFERENCES location,
---     category_id REFERENCES category
--- );
- -- create table location(
--- id SERIAL PRIMARY KEY,
--- location TEXT NOT NULL,
--- preferred_region TEXT NOT NULL
--- )
- -- create table category(
--- id SERIAL PRIMARY KEY,
--- category_name TEXT NOT NULL,
- -- )
- --  -- 3. Soccer League
+ CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    preferred_region_id TEXT REFERENCES location
+);
+ CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    region TEXT,
+    title TEXT,
+    content TEXT,
+    username REFERENCES users,
+    location_id INTEGER REFERENCES location,
+    category_id REFERENCES categories
+);
+ CREATE TABLE location (
+    id SERIAL PRIMARY KEY,
+    location TEXT NOT NULL,
+    preferred_region TEXT NOT NULL
+)
+ CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    category_name TEXT NOT NULL,
+ );
+
+  -- 3. Soccer League
 --  Design a schema for a simple sports league. Your schema should keep track of
 -- All of the teams in the league
 -- All of the goals scored by every player for each game
@@ -62,24 +58,32 @@
 -- All of the referees who have been part of each game
 -- All of the matches played between teams
 -- All of the start and end dates for season that a league has The standings/rankings of each team in the league (This doesn’t have to be its own table if the data can be captured somehow).
- -- CREATE TABLE league (
---     id SERIAL PRIMARY KEY,
---     teams_id INTEGER REFERENCES team,
---     players_id REFERENCES team
--- );
- -- CREATE TABLE team (
---     id SERIAL PRIMARY KEY,
--- teams_name TEXT,
--- player_name TEXT,
---     num_goals INTEGER
--- );
- -- CREATE TABLE game (
---     id SERIAL PRIMARY KEY,
---     referee TEXT,
---     matches TEXT
- -- );
- -- CREATE TABLE season(
---     id SERIAL PRIMARY KEY,
---     start_date TEXT,
---     end_date TEXT
--- )
+ CREATE TABLE league (
+    id SERIAL PRIMARY KEY,
+    teams_id INTEGER REFERENCES teams,
+    players_id REFERENCES teams
+);
+ CREATE TABLE teams (
+    id SERIAL PRIMARY KEY,
+    teams_name TEXT,
+    player_name TEXT,
+    num_goals REFERENCES goals
+);
+ CREATE TABLE goals (
+    id SERIAL PRIMARY KEY
+    player TEXT REFERENCES teams
+ );
+ CREATE TABLE game (
+    id SERIAL PRIMARY KEY,
+    referee REFERENCES referees,
+    matches TEXT
+    season REFERENCES season
+ );
+ CREATE TABLE season(
+    id SERIAL PRIMARY KEY,
+    start_date TEXT,
+    end_date TEXT
+);
+ CREATE TABLE referees(
+    id SERIAL PRIMARY KEY
+);
